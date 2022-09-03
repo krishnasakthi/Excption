3ca53ef260f7e3eecbd63dcdba0191c4b7ef1447
krishna <krishna@DESKTOP-T51J9PB>
2022-09-03 14:33:58 +0530

    Add statments

---
+++ Myfirst/src/com/first/Test.java
@@ -0,0 +1,12 @@
+package com.first;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to STS");
+		System.out.println("welcome to STS in github");
+	}
+
+}
